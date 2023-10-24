1708d85a375eb54081b9ecdb7ff878d392a63b2d
Arlen Neylon <aneylon@scramsystems.com>
2023-10-24 16:44:39 -0600

    Begin setup.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'HonorAndGlory'`, () => {
+  it(`should have as title 'Honor and Glory'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('HonorAndGlory');
+    expect(app.title).toEqual('Honor and Glory');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('HonorAndGlory app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'HonorAndGlory app is running!'
+    );
   });
 });

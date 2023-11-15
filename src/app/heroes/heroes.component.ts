69c73196a4bfc56a9bfa3a5f7c1a698e087bf0ad
Arlen Neylon <aneylon@scramsystems.com>
2023-11-15 09:57:42 -0700

    Add ability to add new heroes.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -29,6 +29,16 @@ export class HeroesComponent {
   //   this.messageService.add(`HeroesComponent: selected hero id=${hero.id}`);
   // }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
